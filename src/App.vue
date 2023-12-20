5feb3de62aed94a77ee3293477a885f498386962
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 16:09:27 -0300

    feat[App]: insert atom component 'LoadingApp' in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-	<LoadingApp />
+	<LoadingApp :active="loading"/>
 	<ViewHome />
 </template>
 
 <script>
 import LoadingApp from "./components/atoms/loading/LoadingApp"
 import ViewHome from "./view/ViewHome.vue"
+import { useCalculatorStore } from "./stores/calculatorStore";
 
 export default {
 	name: "App",
 	components: {
 		ViewHome,
 		LoadingApp
+	},
+	setup() {
+		const calculatorStore = useCalculatorStore()
+
+		return {
+			calculatorStore
+		}
+	},
+	computed: {
+		loading() {
+			return this.calculatorStore.getLoadingRequest
+		}
+	},
+	mounted() {
+		console.log(this.loading)
+		setTimeout(() => {
+			this.lalala = false
+		}, 5000)
 	}
 }
 </script>
