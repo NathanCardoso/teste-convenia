c4d2d9ec2161587e2ff548d37dfd3004ad5dc87f
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 14:25:00 -0300

    feat[View]: created view component 'ViewHome.vue'

---
+++ src/view/ViewHome.vue
@@ -0,0 +1,16 @@
+<template>
+	<TheHeader />
+</template>
+
+<script>
+import TheHeader from "../components/molecules/TheHeader.vue"
+export default {
+	name: "ViewHome",
+	components: {
+		TheHeader
+	}
+}
+</script>
+
+<style scoped>
+</style>
