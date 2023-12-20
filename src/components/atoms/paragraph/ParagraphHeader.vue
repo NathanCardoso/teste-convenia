aab49b2b8daa4dfffa431ddd016ec5e199fda94d
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 14:19:23 -0300

    feat[Paragraph]: created atom component 'ParagraphHeader.vue'

---
+++ src/components/atoms/paragraph/ParagraphHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="header__paragraph">Calculadora de valores</p>
+</template>
+
+<script>
+export default {
+  name: "ParagraphHeader",
+  functional: true,
+};
+</script>
+
+<style scoped>
+.header__paragraph {
+  color: var(--purplePrimary);
+  font-size: 1rem;
+}
+</style>
