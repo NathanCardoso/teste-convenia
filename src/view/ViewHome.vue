e59b8641dadcf33a5119e920520eb53ca5734773
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 15:18:57 -0300

    feat[View]: insert component 'FormApp.vue'

--- src/view/ViewHome.vue
+++ src/view/ViewHome.vue
@@ -1,16 +1,42 @@
 <template>
-	<TheHeader />
+  <TheHeader />
+  <div class="page-home">
+    <FormApp  class="animeLeft" />
+  </div>
 </template>
 
 <script>
-import TheHeader from "../components/molecules/TheHeader.vue"
+import TheHeader from "../components/molecules/TheHeader.vue";
+import FormApp from "../components/organisms/FormApp.vue";
+
 export default {
-	name: "ViewHome",
-	components: {
-		TheHeader
-	}
-}
+  name: "ViewHome",
+  components: {
+    TheHeader,
+    FormApp,
+  },
+};
 </script>
 
 <style scoped>
+.page-app {
+  display: grid;
+  grid-template-columns: 20rem 1fr;
+  gap: 1rem;
+  margin: 1.25rem 0;
+  flex: 1;
+}
+
+@media screen and (max-width: 767px) {
+  .page-app {
+    display: flex;
+    flex-direction: column;
+  }
+  .page-app .input-content {
+    display: block;
+  }
+  .page-app .output-content {
+    display: none;
+  }
+}
 </style>
