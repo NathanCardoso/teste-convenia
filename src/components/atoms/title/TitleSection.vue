373ded0fce4dd4af0c5b3abd0c3ec9c932de467e
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 15:29:00 -0300

    feat[Atom]: created component 'TitleSection.vue'

---
+++ src/components/atoms/title/TitleSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="title-section">
+    <h2 class="title-section__title">{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TitleSection",
+  props: {
+    message: {
+      type: String,
+      default: "insert section text here",
+      required: true,
+    },
+  },
+  functional: true,
+};
+</script>
+
+<style scoped>
+.title-section__title {
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: var(--purplePrimary);
+}
+</style>
