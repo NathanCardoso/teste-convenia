a2c1dd396774b690c9e42594ba8ce76a31be6b22
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 15:31:21 -0300

    feat[Molecules]: created component 'BoxOutResult.vue'

---
+++ src/components/molecules/BoxOutResult.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box-result">
+    <slot name="paragraph"></slot>
+    <slot name="money-value"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoxOutResult",
+	functional: true
+};
+</script>
+
+<style scoped>
+.box-result {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 1.25rem 1.875rem;
+}
+
+@media screen and (max-width: 767px) {
+  .box-result {
+    padding: 1.25rem 0;
+  }
+}
+</style>
