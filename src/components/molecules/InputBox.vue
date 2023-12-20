f5f96c84aa5b4c0e5e71f4ae4e8810ab010a18b3
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 14:47:57 -0300

    feat[Molecule]: created molecule component 'InputBox.vue'

---
+++ src/components/molecules/InputBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="input-box">
+    <ParagraphApp :message="message" v-if="!!message" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import ParagraphApp from "../atoms/paragraph/ParagraphApp.vue";
+
+export default {
+  name: "InputBox",
+  props: {
+    message: {
+      type: String,
+    },
+  },
+  components: {
+    ParagraphApp,
+  },
+};
+</script>
+
+<style scoped>
+.input-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  background: var(--whiteSecondary);
+  padding: 0.5rem 3.625rem;
+  min-height: 5rem;
+  border-radius: 0.625rem;
+  box-sizing: border-box;
+  flex: 1;
+}
+
+@media screen and (max-width: 767px) {
+  .input-box {
+    padding: 0.5rem 0.625rem;
+  }
+}
+</style>
