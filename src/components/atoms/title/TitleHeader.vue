831c6249158b229623668386328093a97f4d8ff0
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2023-12-20 14:16:54 -0300

    feat[title]: created atom component 'TitleHeader.vue'

---
+++ src/components/atoms/title/TitleHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="header__title">Le Tip</h1>
+</template>
+
+<script>
+export default {
+  name: "TitleHeader",
+  functional: true,
+};
+</script>
+<style scoped>
+.header__title {
+  color: var(--purplePrimary);
+  font-size: 1.625rem;
+  font-weight: 700;
+}
+</style>
